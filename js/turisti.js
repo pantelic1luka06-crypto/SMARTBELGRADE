c574230e8be6e031c20116a1d86eb5dd99deac68
Luka Pantelic <pantelic1.luka06@gmail.com>
2026-02-12 02:42:29 +0100

    ..
    ..

--- js/turisti.js
+++ js/turisti.js
@@ -8,12 +8,71 @@
     texts[0] = "Kalemegdan je istorijska tvrđava i najveći gradski park u Beogradu, sa bogatom prošlošću koja seže vekovima unazad. Sa njegovih zidina pruža se prelep pogled na ušće Save u Dunav i panoramu čitavog grada. U parku se nalaze spomenici, muzeji, kafići i prostrani travnjaci za šetnju i odmor. Kalemegdan je mesto gde se priroda i istorija savršeno spajaju, idealno za porodične izlete ili romantične šetnje. Poseta ovom mestu pruža jedinstvenu priliku da se oseti duh Beograda.";
     texts[1] = "Gardoš je istorijski deo Zemuna, poznat po uskim, kaldrmisanim ulicama i šarmantnoj starogradskoj atmosferi. Sa vidikovca Gardoša pruža se impresivan pogled na Dunav i moderne delove Beograda. U ovom kraju nalaze se autentične kafane, galerije i spomenici koji pričaju priče iz prošlosti. Šetnja Gardošom je poput putovanja kroz vreme, sa svakim korakom koji otkriva deo istorije i kulture. Idealno je mesto za one koji žele mirnu, ali inspirativnu šetnju i lokalni duh grada.";
     texts[2] = "Hram Svetog Save je jedan od najvećih pravoslavnih hramova na svetu i simbol Beograda. Njegova impozantna kupola dominira gradskim horizontom i može se videti sa mnogih delova grada. Unutrašnjost hrama krase raskošni mozaici i freske sa bogatom ikonografijom, a mir i atmosfera prostora ostavljaju snažan utisak. Hram je mesto duhovnosti, kulturnog nasleđa i umetničke vrednosti. Poseta Hramu Svetog Save pruža ne samo vizuelni užitak, već i inspiraciju i osećaj povezanosti sa istorijom i tradicijom Srbije.";
+    
+    let klub = new Array();
+    klub[0] = ""
+    klub[1] = ""
+    klub[2] = ""
 
+    let kafana = new Array();
+    kafana[0] = ""
+    kafana[1] = ""
+    kafana[2] = ""
+
+    let opis = new Array();
+    opis[0] = ""
+    opis[1] = ""
+    opis[2] = ""
+
+    let opis1 = new Array();
+    opis1[0] = ""
+    opis1[1] = ""
+    opis1[2] = ""
+    
     let index = 0;
-
+    const kfel = document.getElementById("kafanas");
+    const op1el = document.getElementById("kafana");
+    const klel = document.getElementById("klubs");
+    const opel = document.getElementById("klub");
     const imgEl = document.getElementById("slideImage");
     const textEl = document.getElementById("slideText");
-
+    
+function prikaz1(i) {
+    kfel.src = kafana[i];
+    op1el.textContent = opis1[i];
+}
+function sldc1() {
+    index++;
+    if (index >= kafana.length) {
+        index = 0;
+    }
+    prikaz1(index);
+}
+function prosli1() {
+    index--;
+    if (index < 0) {
+        index = kafana.length - 1;
+    }
+    prikaz1(index);
+}
+    function prikaz(i){
+        klel.src=klub[i];
+        opel.textContent=opis[i];
+    }
+    function sldc(){
+        index++;
+        if(index >=klub.length){
+            index=0;
+        }
+        prikaz(index);
+    }
+    function prosli(){
+        index--;
+        if (index<0){
+            index=klub.length - 1;
+        }
+        prikaz(index);
+    }
     function showSlide(i) {
         imgEl.src = images[i];
     textEl.textContent = texts[i];
